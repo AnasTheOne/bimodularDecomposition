d4e0ab03dc95ac5c099ef154aa99f2b857d3a922
AnasTheOne <40430115+AnasTheOne@users.noreply.github.com>
2021-03-27 01:46:09 +0100

    Update Bimodul.py
    Correcting connectivity

--- Bimodul.py
+++ Bimodul.py
@@ -1,78 +1,149 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+
+f = lambda x : chr(x+ord('a'))
+lenL = lambda L : len(L[0])+len(L[1])
 
 #Calcul d'une seconde represontation du bipari
-def secondRep(G,n,m):
-    res = [[] for i in range(m)]
-    for i in range(n):
-        for x in G[i]:
+def Gb(Gw,n):
+    res = [[] for i in range(n[1])]
+    for i in range(n[0]):
+        for x in Gw[i]:
             res[x].append(i)
     return res
 
-def exploreB(v,V,comp):
-    flagsOnG[v] = 1
-    for x in Gb[v-n]:
-        if flagsOnG[x+n] == 0 and V[x+n] == 1:
-            flagsOnG[x+n] = 1
-            comp.add(x+n)
-            exploreW(x,V,comp) 
+def complement(G):
+    res = [[] for i in range(n[1])]
+    for i in range(n[0]):
+        for j in range(n[1]):
+            if not j in G[i]:
+                res[i].append(j)
+    return res
 
-def exploreW(v,V,comp):
-    flagsOnG[v] = 1
-    for x in Gw[v]:
-        if flagsOnG[x] == 0 and V[x] == 1:
-            flagsOnG[x] = 1
-            comp.add(x)
-            exploreB(x,V,comp)
+def randomGen(n,p):
+    G = []
+    for i in range(n[0]):
+        L = []
+        for j in range(n[1]):
+            if random.random() < p:
+                L.append(j)
+        G+=[L]
+    return G
 
-#Composantes connexes de G et de G_barre restreints à V
-def connectedComps(V):
-    l = 0
-    flagRoot = True
-    for i in range(n+m):
-        if flagRoot and V[i] == 1:
-            root = i
-            flagRoot = False
-        l += V[i]
-    comp = set()
-    comp.add(root)
-    if root < n:
-        exploreW(root,V,comp)
-    else:
-        exploreB(root,V,comp)
-    if sum(flagsOnG) < l:
-        for x in comp:
-            V[x] = 0
-        return [comp] + connectedComps(V)
-    else:
-        return [comp]
 
-n,m = 10,10
-Gw = [[0],[1,2,3],[1,2],[3,4],[4,5],[3,5],[6,7,8,9],[7,8],[8,9],[9,6]]
-Gb = secondRep(Gw,n,m)
-subV = [0 for i in range(n+m)]
-flagsOnG = [0 for i in range(n+m)]
-flagsOnG_b = [0 for i in range(n+m)] #à remettre à zero après chaque décomposition
+
+def connecetedComps(V):
+    toVisit = [[],[]]
+    flagsVisited = [[0 for i in range(n[0])],[0 for i in range(n[1])]]
+    counter = [0,0]
+    comps = []
+
+    while counter[0]+counter[1] < N:
+        c = 0
+        if counter[0] == n[0]:
+            c = 1
+        
+        for i in range(n[c]):
+            if flagsVisited[c][i]==0 and V[c][i]==1:
+                toVisit[c].append(i)
+                break
+
+        comp = set()
+        while lenL(toVisit)>0:
+            c = 0
+            if len(toVisit[c]) == 0:
+                c=1
+            v = toVisit[c].pop(0)
+            if flagsVisited[c][v] == 0:
+                flagsVisited[c][v] = 1
+                counter[c] += 1
+                comp.add(v+n[0]*c)
+                for x in G[c][v]:
+                    if flagsVisited[1-c][x] == 0 and V[1-c][x]==1:
+                        toVisit[1-c].append(x)
+                
+        comps += [comp]
+    return comps
+    
+def connecetedComps_b(V):
+    toVisit = [[],[]]
+    flagsVisited = [[0 for i in range(n[0])],[0 for i in range(n[1])]]
+    counter = [0,0]
+    comps = []
+
+    while counter[0]+counter[1] < N:
+        c = 0
+        if counter[0] == n[0]:
+            c = 1
+        
+        for i in range(n[c]):
+            if flagsVisited[c][i]==0 and V[c][i]==1:
+                toVisit[c].append(i)
+                break
+
+        comp = set()
+        while lenL(toVisit)>0:
+            c = 0
+            if len(toVisit[c]) == 0:
+                c=1
+            v = toVisit[c].pop(0)
+            if flagsVisited[c][v] == 0:
+                flagsVisited[c][v] = 1
+                counter[c] += 1
+                comp.add(v+n[0]*c)
+                for x in range(n[1-c]):
+                    if not x in G[c][v]:
+                        if flagsVisited[1-c][x] == 0 and V[1-c][x]==1:
+                            toVisit[1-c].append(x)
+                
+        comps += [comp]
+    return comps
+    
+
+n = [10,10]
+N = n[0]+n[1]
+#Gw = [[0],[1,2,3],[1,2],[3,4],[4,5],[3,5],[6,7,8,9],[7,8],[8,9],[9,6]]
+#Gw = [[5], [0, 1, 8], [0, 8], [0, 3, 4, 8], [1, 3], [4, 7, 8, 9], [7], [7, 8, 9], [5, 6], [0, 3, 6, 8]]
+#Gw = [[0, 1, 2, 3, 4, 5, 7, 8, 9], [0, 1, 2, 3, 5, 6, 8, 9], [2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 4, 5, 7, 8], [0, 2, 3, 4, 6, 7, 8, 9], [0, 1, 3, 4, 5, 7, 8, 9], [0, 1, 3, 4, 5, 6, 7, 8, 9], [0, 2, 3, 4, 6, 7, 8, 9], [1, 4, 5, 6, 7, 8, 9], [0, 1, 2, 4, 5, 6, 7, 8, 9]]
+Gw = randomGen(n,0.8)
+G=[Gw,Gb(Gw,n)]
 
 print(Gw)
-print(Gb)
-comps = connectedComps([1 for i in range(n+m)])
-print(comps)
-"""
-G = nx.bipartite.gnmk_random_graph(4, 4, 10, seed=123)
-top = nx.bipartite.sets(G)[0]
-pos = nx.bipartite_layout(G, top)
-nx.draw(G, pos=pos)
-plt.show()
-"""
+coms = connecetedComps([[1 for i in range(n[0])],[1 for i in range(n[1])]])
+print("BFS: ", coms)
+coms_b = connecetedComps_b([[1 for i in range(n[0])],[1 for i in range(n[1])]])
+print("BFS_b: ", coms_b)
 
+#affichage
 G = nx.Graph()
-f = lambda x : chr(x+ord('a'))
-G.add_nodes_from([i for i in range(n)])
-G.add_nodes_from([f(i) for i in range(m)])
-for i in range(n):
+G.add_nodes_from([i for i in range(n[0])])
+G.add_nodes_from([f(i) for i in range(n[1])])
+for i in range(n[0]):
     for x in Gw[i]:
         G.add_edge(i, f(x))
-colors = ['#DDDDDD' for i in range(n)]+['#999999' for i in range(m)]
+
+Gw = complement(Gw)
+G_b=[Gw,Gb(Gw,n)]
+G_barre = nx.Graph()
+G_barre.add_nodes_from([i for i in range(n[0])])
+G_barre.add_nodes_from([f(i) for i in range(n[1])])
+for i in range(n[0]):
+    for x in Gw[i]:
+        G_barre.add_edge(i, f(x))
+
+colors = ['#DDDDDD' for i in range(n[0])]+['#999999' for i in range(n[1])]
+color = ['#FF0000','#FFFF00','#0000FF','#00FFFF','#FF00FF','#00FF00','#EEEEEE','#555511','#115555','#551155','#111155','#115511','#551111',]
+r = random.randint(0, 10)
+for i in range(len(coms)):
+    for y in coms[i]:
+        colors[y] = color[(i+r)%len(color)]
+plt.figure()
 nx.draw(G, with_labels=True, font_weight='bold',node_color=colors)
+plt.figure()
+r = random.randint(0, 10)
+for i in range(len(coms_b)):
+    for y in coms_b[i]:
+        colors[y] = color[(i+r)%len(color)]
+nx.draw(G_barre, with_labels=True, font_weight='bold',node_color=colors)
 plt.show()
